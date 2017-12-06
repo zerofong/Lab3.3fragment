d0cef166f949205cce1e7b3be93d87e1cd456483
taruc <h_fong97@hotmail.com>
2017-12-06 14:05:54 +0800

    Initial commit

---
+++ app/src/main/java/my/edu/tarc/lab33/GalleryFragment.java
@@ -0,0 +1,66 @@
+package my.edu.tarc.lab33;
+
+
+import android.content.Intent;
+import android.media.Image;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import static android.app.Activity.RESULT_OK;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class GalleryFragment extends Fragment {
+
+    private static final int REQUEST_PHOTO = 1;
+    private ImageView imageViewPhoto;
+
+    public GalleryFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_gallery, container, false);
+        //Link UI to program
+        imageViewPhoto = (ImageView)v.findViewById(R.id.imageViewPhoto);
+        Button buttonViewImage = (Button)v.findViewById(R.id.buttonViewImage);
+        buttonViewImage.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+
+                //Filter to only show results that can be "opened"
+                intent.addCategory(Intent.CATEGORY_OPENABLE);
+
+                //Filter to show only images, using the image MIME data
+                intent.setType("image/*");
+
+                startActivityForResult(intent,REQUEST_PHOTO);
+
+            }
+        });
+        // Inflate the layout for this fragment
+        return v;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == REQUEST_PHOTO && resultCode == RESULT_OK){
+            if(data != null){
+                Uri uri;
+                uri = data.getData();
+                imageViewPhoto.setImageURI(uri);
+            }
+        }
+    }
+}
